d95f74e14e0a3e6f68fa407f20144d13b14ec402
GURKIRAT <gs544448@gmail.com>
2024-10-23 13:32:24 +0530

    Update F1.cpp

--- F1.cpp
+++ F1.cpp
@@ -1,27 +1,25 @@
-#include<iostream>
-#include<fstream>
-//the useful classes for working in cppar:
-//1.fstreambase class
-//2.ifstream class->derived from fstreambase
-//3.ofstream class->derived from fstreambase
-//inorder to work with files in cpp,you will have to open it
-//there are 2 ways to open it
-//1.using the constructor
-//2.using the member function open() of class
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-int main()
-{
-  string st ="Garry Bhai";
-  string st2;
-  // //opening files using constructor and writing it
-  // ofstream out("sample.txt");//write operation
-  // out<<st;
-  //opening file using constructor and reading it
-  ifstream in("sample1.txt");//read operations
-  //in>>st2;//this will give only first word of the statement
-  getline(in,st2);//entire statement
-  getline(in,st2);//entire next statement
-  cout<<st2;
-  
-  return 0;
+
+int main() {
+    // Write operation
+    ofstream hout("sample.txt"); // Create and open the file
+    string st;
+    
+    cout << "Enter your name: "; // Prompt user for input
+    cin >> st; // Read user input
+    hout << "my name is " << st; // Write to file
+    hout.close(); // Close the file
+
+    // Read operation
+    ifstream hin("sample.txt"); // Open the file for reading
+    string content;
+    
+    getline(hin, content); // Read the entire line
+    cout << "Content of this file is: " << content << endl; // Output the content
+    hin.close(); // Close the file
+
+    return 0;
 }
